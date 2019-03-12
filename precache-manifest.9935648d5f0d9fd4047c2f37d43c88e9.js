504583c6a30c2647b8bcc2446a183d99063b10b4
Palash Gupta <palashg7563@gmail.com>
2019-03-12 11:40:40 +0530

    Updates

---
+++ precache-manifest.9935648d5f0d9fd4047c2f37d43c88e9.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d6a1baf3a3213dae018",
+    "url": "/campk12-design/static/css/main.1d2d848f.chunk.css"
+  },
+  {
+    "revision": "6d6a1baf3a3213dae018",
+    "url": "/campk12-design/static/js/main.6d6a1baf.chunk.js"
+  },
+  {
+    "revision": "31858e1886661671592f",
+    "url": "/campk12-design/static/js/runtime~main.31858e18.js"
+  },
+  {
+    "revision": "a2f4a10972a392e5c51c",
+    "url": "/campk12-design/static/js/2.a2f4a109.chunk.js"
+  },
+  {
+    "revision": "8a6575a0f5f909e6198be02d1ac0b80e",
+    "url": "/campk12-design/static/media/skill-icon.8a6575a0.svg"
+  },
+  {
+    "revision": "85e63e995ad6d3629dc10203f821c785",
+    "url": "/campk12-design/static/media/coins.85e63e99.svg"
+  },
+  {
+    "revision": "367f56597468e989748c21ea13019df2",
+    "url": "/campk12-design/static/media/web-development-icon.367f5659.svg"
+  },
+  {
+    "revision": "264223a160bf7847ee451f4a9428e743",
+    "url": "/campk12-design/static/media/ai-icon.264223a1.svg"
+  },
+  {
+    "revision": "926788b30085305d754d3bffff54e0d6",
+    "url": "/campk12-design/static/media/virtual-reality-icon.926788b3.svg"
+  },
+  {
+    "revision": "7d07925be7f2e845d40d333a76fc6604",
+    "url": "/campk12-design/static/media/app-development-icon.7d07925b.svg"
+  },
+  {
+    "revision": "b06a4e43591739579c3825021e21c646",
+    "url": "/campk12-design/static/media/left-arrow-icon.b06a4e43.svg"
+  },
+  {
+    "revision": "ac6fc2cdff3a0dc749af63a9b72144e4",
+    "url": "/campk12-design/static/media/right-arrow-icon.ac6fc2cd.svg"
+  },
+  {
+    "revision": "5473eee1ba2a0c2921b27192cdc990a7",
+    "url": "/campk12-design/static/media/logo.5473eee1.svg"
+  },
+  {
+    "revision": "6238349bb0f5e4c3557a691668356454",
+    "url": "/campk12-design/static/media/Notification.6238349b.svg"
+  },
+  {
+    "revision": "3db344eef9ebfb12a588041e6faa9385",
+    "url": "/campk12-design/static/media/LeaderBoard.3db344ee.svg"
+  },
+  {
+    "revision": "fdc617d84744194e9440ead72fd6bb07",
+    "url": "/campk12-design/static/media/arrow.fdc617d8.svg"
+  },
+  {
+    "revision": "9e566b6e6c6ce5e6714b7cb249067b3d",
+    "url": "/campk12-design/static/media/grades.9e566b6e.svg"
+  },
+  {
+    "revision": "1a5b6fb76660013947d94daa4badfcc4",
+    "url": "/campk12-design/static/media/pre-req.1a5b6fb7.svg"
+  },
+  {
+    "revision": "b07fa977b4f4b992169a4e96e1ef4dea",
+    "url": "/campk12-design/static/media/calendarIcon.b07fa977.svg"
+  },
+  {
+    "revision": "9ffb5966364cd7214c93955e5fcf970c",
+    "url": "/campk12-design/static/media/dayicon.9ffb5966.svg"
+  },
+  {
+    "revision": "968a99b6a96b96133b71c3704e1ff112",
+    "url": "/campk12-design/static/media/timeicon.968a99b6.svg"
+  },
+  {
+    "revision": "81fa54df9a01ada505a3244dd55d0812",
+    "url": "/campk12-design/static/media/header-photo.81fa54df.jpg"
+  },
+  {
+    "revision": "1960d6a42bf465dd20b7f671f534e3f7",
+    "url": "/campk12-design/static/media/Header.1960d6a4.svg"
+  },
+  {
+    "revision": "704ff7142970147c71de3b56624fd7b4",
+    "url": "/campk12-design/index.html"
+  }
+];
